55e2247fdd0e1fb290f887276054f40eaae70930
harsh istalkar <harshistalkar1@gmail.com>
2023-09-15 11:46:47 +0200

    Working Score and death UI

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField]
+    public string PlayerRightTag = "PlayerRight";
+    [SerializeField]
+    public string PlayerLeftTag = "PlayerLeft";
+    [SerializeField]
+    public string ReflectWallTag = "ReflectWall";
+    [SerializeField]
+    public string DeathWallLeftTag = "DeathWallLeft";
+    [SerializeField]
+    public string DeathWallRightTag = "DeathWallRight";
+    
+    [SerializeField]
     public float MovementSpeed = 15.0f;
+    
+    [SerializeField]
+    public TextMeshProUGUI PlayerLeftScoreText;
+    [SerializeField]
+    public TextMeshProUGUI PlayerRightScoreText;
+    
+    private int PlayerLeftScore = 0;
+    private int PlayerRightScore = 0;
+    
+    private bool LastPlayerHitWasRight = false;
+    
     private Vector2 MoveDirection = Vector2.zero;
     // Start is called before the first frame update
     void Start()
@@ -31,22 +55,58 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("ReflectWall"))
+        if (other.gameObject.CompareTag(ReflectWallTag))
         {
-            Debug.Log("Reflection collision");
             MoveDirection.y = -MoveDirection.y;
+            if (MoveDirection.x>0)
+            {
+                MoveDirection.x+= Random.Range(0.05f, 0.01f);
+            }
+            else
+            {
+                MoveDirection.x-= Random.Range(0.05f, 0.01f);
+            }
+            MoveDirection = MoveDirection.normalized;
         }
-        else if (other.gameObject.CompareTag("Player"))
+        else if (other.gameObject.CompareTag(PlayerRightTag))
         {
             MoveDirection = MoveDirection - 2*(Vector2.Dot(MoveDirection, other.contacts[0].normal)) * other.contacts[0].normal;
+            PlayerRightScore++;
+            if (PlayerRightScoreText!=null)
+            {
+                PlayerRightScoreText.text = PlayerRightScore.ToString();
+            }
+            LastPlayerHitWasRight = true;
         }
-        else if (other.gameObject.CompareTag("DeathWallLeft"))
+        else if (other.gameObject.CompareTag(PlayerLeftTag))
         {
-            Debug.Log("PlayerLeft Lost");
+            MoveDirection = MoveDirection - 2*(Vector2.Dot(MoveDirection, other.contacts[0].normal)) * other.contacts[0].normal;
+            PlayerLeftScore++;
+            if (PlayerLeftScoreText!=null)
+            {
+                Debug.Log("PlayerLeftScore: " + PlayerLeftScore);
+                PlayerLeftScoreText.text = PlayerLeftScore.ToString();
+            }
+            LastPlayerHitWasRight = false;
         }
-        else if (other.gameObject.CompareTag("DeathWallRight"))
+        else if (other.gameObject.CompareTag(DeathWallLeftTag))
         {
-            Debug.Log("PlayerRight Lost");
+            if (!LastPlayerHitWasRight)
+            {
+                PlayerLeftScore--;
+            }
+            PlayerLeftScoreText.text = "Left Lost. Score: " + PlayerLeftScore.ToString();
+            Time.timeScale = 0;
+        }
+        else if (other.gameObject.CompareTag(DeathWallRightTag))
+        {
+            if (LastPlayerHitWasRight)
+            {
+                PlayerRightScore--;
+            }
+            PlayerRightScoreText.text = "Right Lost. Score: " + PlayerRightScore.ToString();
+            Time.timeScale = 0;
+            
         }
     }
     
